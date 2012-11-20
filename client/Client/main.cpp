aee34b9d5bd16b8502a9f91c9a461654fb8d0e1c
dportnoy <dmp1488@gmail.com>
2012-11-19 20:19:44 -0500

    Cleaned up the client a bit and made it more c++-like and added to the readme

--- client/Client/main.cpp
+++ client/Client/main.cpp
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 
 #include <winsock2.h>
+#include <WS2tcpip.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,14 +32,13 @@ int boost_main()
 
 int main(int argc, char *argv[])
 {
-   int sock, n;
-   int length;
-   struct sockaddr_in server, from;
-   struct hostent *hp;
-   char buffer[256];
-   
+	int sock, n;
+	struct sockaddr_in server, from;
+	struct hostent *hp;
+	char buffer[256];
+
 	if (argc != 3) {
-		printf("Usage: server port\n");
+		cout << "Usage: server port" << endl;
 		exit(1);
 	}
 
@@ -50,32 +50,36 @@ int main(int argc, char *argv[])
 	wsaerr = WSAStartup(wVersionRequested, &wsaData);
 	
 	if (wsaerr != 0) {
-		printf("The Winsock dll not found.\n");
+		cout << "The Winsock dll not found." << endl;
 		exit(1);
 	}else
-		printf("The Winsock dll was found.\n");
+		cout << "The Winsock dll was found." << endl;
 	
 	sock= socket(AF_INET, SOCK_DGRAM, 0);
-	if (sock < 0) error("socket");
+	if (sock < 0)
+		error("socket");
 
 	server.sin_family = AF_INET;
 	hp = gethostbyname(argv[1]);
-	if (hp==0) error("Unknown host");
+	if (hp==0)
+		error("Unknown host");
 
-	memcpy((char *)&server.sin_addr,
-			(char *)hp->h_addr,
-			hp->h_length);
-   server.sin_port = htons(atoi(argv[2]));
-   length=sizeof(struct sockaddr_in);
-   printf("Please enter the message: ");
-   memset(buffer, 0, 256);
-   fgets(buffer,255,stdin);
-   n=sendto(sock,buffer,
-            strlen(buffer),0,(const struct sockaddr *)&server,length);
-	if (n < 0) error("Sendto");
-	n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
-	if (n < 0) error("recvfrom");
+	memcpy((char *)&server.sin_addr, (char *)hp->h_addr, hp->h_length);
+	server.sin_port = htons(atoi(argv[2]));
+	cout << "Please enter the message: ";
+	memset(buffer, 0, 256);
+	fgets(buffer,255,stdin);
 
+	socklen_t fromlen = sizeof(server);
+
+	n=sendto(sock,buffer, strlen(buffer), 0, (const struct sockaddr *)&server, fromlen);
+	if (n < 0)
+		error("Sendto");
+
+	n = recvfrom(sock, buffer, 256, 0, (struct sockaddr *)&from, &fromlen);
+	if (n < 0)
+		error("recvfrom");
+	
 	buffer[n] = '\0';
 	cout << "Got an ack: " << endl;
 	cout << buffer << endl;
--- readme.txt
+++ readme.txt
@@ -2,3 +2,23 @@ BoostPro Installer options
 
 -multithreaded
 -multithreaded debug
+
+
+old installation instructions
+
+client
+______
+
+install the boost library
+install openssl using the old windows installer
+
+Add the following to Linker -> Input -> Additional Dependencies:
+libeay32.lib
+ssleay32.lib
+
+
+server
+______
+
+install the boost library
+install openssl on ubuntu (should be easy)
