9c83610b7bea1f725f9a7238a002aa37b0e98075
dportnoy <dmp1488@gmail.com>
2012-12-30 19:19:04 +0000

    Added debug messages to Message.cpp

--- common/Message.cpp
+++ common/Message.cpp
@@ -10,9 +10,15 @@
    #include <netinet/in.h>
 #endif
 
+#include <iostream>
+
+using namespace std;
+
 int sendMessage(NETWORK_MSG *msg, int sock, struct sockaddr_in *dest)
 {
    return sendto(sock, (char*)msg, sizeof(NETWORK_MSG), 0, (struct sockaddr *)dest, sizeof(struct sockaddr_in));
+
+   cout << "Sent message of type " << msg->type << endl;
 }
 
 int receiveMessage(NETWORK_MSG *msg, int sock, struct sockaddr_in *dest)
@@ -21,4 +27,6 @@ int receiveMessage(NETWORK_MSG *msg, int sock, struct sockaddr_in *dest)
 
    // assume we don't care about the value of socklen
    return recvfrom(sock, (char*)msg, sizeof(NETWORK_MSG), 0, (struct sockaddr *)dest, &socklen);
+
+   cout << "Received message of type " << msg->type << endl;
 }
